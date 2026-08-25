4dacd21d281898b5d0f0f4df941f765254150384
Sualeh Fatehi <sualeh@hotmail.com>
2026-08-25 01:46:05 +0000

    SchemaCrawler v17.14.1

--- Formula/schemacrawler.rb
+++ Formula/schemacrawler.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.26.0-SNAPSHOT at 2026-07-20T16:57:23.1010183Z
+# Generated with JReleaser 1.26.0-SNAPSHOT at 2026-08-25T01:45:25.2197021Z
 
 class Schemacrawler < Formula
   desc "Free database schema discovery and comprehension tool"
   homepage "https://www.schemacrawler.com/"
-  url "https://github.com/schemacrawler/SchemaCrawler-Installers/releases/download/v17.12.2/schemacrawler-17.12.2-bin.zip"
-  version "17.12.2"
-  sha256 "32151d9155a0b913d15720ae706ffdc662c06bacbbcea8826105eae6201e80b8"
+  url "https://github.com/schemacrawler/SchemaCrawler-Installers/releases/download/v17.14.1/schemacrawler-17.14.1-bin.zip"
+  version "17.14.1"
+  sha256 "0a716b44a7beb7e06f0b34d044b41e5cd03f5214d7ee6cd308a273bb995e71f0"
   license "EPL-1.0"
 
   depends_on "openjdk@17"
@@ -17,6 +17,6 @@ class Schemacrawler < Formula
 
   test do
     output = shell_output("#{bin}/schemacrawler --version")
-    assert_match "17.12.2", output
+    assert_match "17.14.1", output
   end
 end
